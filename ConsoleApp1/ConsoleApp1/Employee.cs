a77ad772c3f7fd511a9cb10f9626f00b5bbd2097
wxc4295 <wxc4295@IGM2435-17>
2018-09-24 12:44:05 -0400

    ...

--- ConsoleApp1/ConsoleApp1/Employee.cs
+++ ConsoleApp1/ConsoleApp1/Employee.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    class Employee
     {
-        static void Main(string[] args)
-        {
-          
-        }
     }
 }
